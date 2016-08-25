cf7c9da1d249a21f09aee1adec9386aade49c9b0
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-25 21:36:55 +0200

    Update iss.sh

--- iss.sh
+++ iss.sh
@@ -5,17 +5,17 @@ case "$1" in
 ch0)
 screen -X -S stream quit
 echo "Set channel auto"
-screen -d -m -S stream sh -c '/home/pi/stream/iss-stream.sh ch0; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch0; exec bash'
 ;;
 ch1)
 screen -X -S stream quit
 echo "Set channel 1"
-screen -d -m -S stream sh -c '/home/pi/stream/iss-stream.sh ch1; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch1; exec bash'
 ;;
 ch2)
 screen -X -S stream quit
 echo "Set channel 2"
-screen -d -m -S stream sh -c '/home/pi/stream/iss-stream.sh ch2; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch2; exec bash'
 ;;
 stop)
 echo "Stream Ended"
