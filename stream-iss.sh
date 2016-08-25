3f759fded10b285c8f690115430b8b71366b670c
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-25 19:42:02 +0200

    path error

--- stream-iss.sh
+++ stream-iss.sh
@@ -5,17 +5,17 @@ case "$1" in
 ch0)
 screen -X -S stream quit
 echo "Set Channel auto"
-screen -d -m -S stream sh -c './iss-stream.sh ch0; exec bash'
+screen -d -m -S stream sh -c './stream/stream-iss0.sh; exec bash'
 ;;
 ch1)
 screen -X -S stream quit
 echo "Set Channel 1"
-screen -d -m -S stream sh -c './iss-stream.sh ch1; exec bash'
+screen -d -m -S stream sh -c './stream/stream-iss1.sh; exec bash'
 ;;
 ch2)
 screen -X -S stream quit
 echo "Set Channel 2"
-screen -d -m -S stream sh -c './iss-stream.sh ch2; exec bash'
+screen -d -m -S stream sh -c './stream/stream-iss2.sh; exec bash'
 ;;
 stop)
 echo "Stream Ended"
@@ -27,4 +27,3 @@ echo "Usage: $(basename $0) {ch0|ch1|ch2|stop}"
 exit 1
 ;;
 esac  
-
