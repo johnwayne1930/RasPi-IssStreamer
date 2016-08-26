accd70d87cf0104fdf5cd59eb7c7d46fb6b10211
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-26 12:46:16 +0200

    added comments

--- iss.sh
+++ iss.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 case "$1" in
-
-ch0)
+ch0) # HDEV ustream channel and Canadarm2/interior ustream channel swap according to day/night time of iss orbit
 screen -X -S stream quit
 echo "Set channel auto"
 screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch0; exec bash'
 ;;
-ch1)
+ch1) # HDEV ustream channel
 screen -X -S stream quit
 echo "Set channel 1"
 screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch1; exec bash'
 ;;
-ch2)
+ch2) # Canadarm2/interior ustream channel
 screen -X -S stream quit
 echo "Set channel 2"
 screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch2; exec bash'
 ;;
-stop)
+stop) # stops all actions without starting new ones
 echo "Stream Ended"
 screen -X -S stream quit
 exit
 ;; 
-*)
+*) # in case the input is wrong
 echo "Usage: $(basename $0) {ch0|ch1|ch2|stop}"
 exit 1
 ;;
