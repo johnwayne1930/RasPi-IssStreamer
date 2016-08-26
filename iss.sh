2d26053197e771736c4d0d42c6413b004fb9fea1
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-26 15:10:35 +0200

    Update iss.sh

--- iss.sh
+++ iss.sh
@@ -4,17 +4,17 @@ case "$1" in
 ch0) # HDEV ustream channel and Canadarm2/interior ustream channel swap according to day/night time of iss orbit
 screen -X -S stream quit
 echo "Set channel auto"
-screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch0; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh "$1"; exec bash'
 ;;
 ch1) # HDEV ustream channel
 screen -X -S stream quit
 echo "Set channel 1"
-screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch1; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh "$1"; exec bash'
 ;;
 ch2) # Canadarm2/interior ustream channel
 screen -X -S stream quit
 echo "Set channel 2"
-screen -d -m -S stream sh -c '~/stream/iss-stream.sh ch2; exec bash'
+screen -d -m -S stream sh -c '~/stream/iss-stream.sh "$1"; exec bash'
 ;;
 stop) # stops all actions without starting new ones
 echo "Stream Ended"
