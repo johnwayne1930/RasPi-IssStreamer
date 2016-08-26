81bb6f9ddab2a78177f930492622ddb403e3b796
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-26 12:22:40 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -3,10 +3,10 @@
 case "$1" in
 ch0)
 while true; do
-visibility=$(python -c 'import iss')
-if [ $visibility = "daylight" ]; then
+visibility=$(python -c 'import iss');
+if [ $visibility = daylight ]; then
  livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
-elif [ $visibility = "eclipsed" ]; then
+elif [ $visibility = eclipsed ]; then
  livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo  --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
 fi
 done &
