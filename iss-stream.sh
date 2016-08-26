150672aab77f15bee7b838c6931c774e44842db5
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-26 12:41:52 +0200

    added comments

--- iss-stream.sh
+++ iss-stream.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# keeps the livestream up and running
 while true; do
 case "$1" in
 ch0)
@@ -19,11 +20,13 @@ livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxp
 esac
 done &
 
+# orbit picture for the background
 while sleep 60; do 
 wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"
 DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/bin/feh -F -Z /tmp/issorbit.png
 done &
 
+# orbit overlay in top left corner; have to simplify this..
 wget -O /tmp/ISS-Display-image1.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=150&height=150&satid=25544"
 avconv -loop 1 -i /tmp/ISS-Display-image1.png -tune stillimage -t 10 -y -r 5 /tmp/ISS-Display-video1.mp4
  
