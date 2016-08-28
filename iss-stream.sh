34d09c4ee8bbc0f2c60ea219fb9d58d4dbf3c68d
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-28 12:04:51 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -23,6 +23,7 @@ esac
 done &
 
 # orbit picture for the background
+wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"
 DISPLAY=:0.0 XAUTHORITY=~/.Xauthority /usr/bin/feh -F -Z -R 30 /tmp/issorbit.png &
 while sleep 60; do wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"; done &
 
