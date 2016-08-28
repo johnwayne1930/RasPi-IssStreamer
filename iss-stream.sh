6bde7c2ee0209f2da7f80050c67a8614c4a447ba
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-28 13:24:34 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -7,11 +7,13 @@ ch0)
 ~/stream/iss.py > /tmp/iss.log
 sleep 3
 visibility=$(</tmp/iss.log)
- if [ $visibility != eclipsed ]; then
-  /usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
- else
-  /usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo  --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
- fi
+if [ "$visibility" != "$visold" ]; then pkill -f livestreamer; fi
+visold=$visibility
+if [ $visibility != eclipsed ]; then
+ /usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
+else
+ /usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo  --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
+fi
 ;;
 ch1)
 /usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
