b52f14aff4da3838a03f85e9a9b2e06b50a99b18
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-08-28 15:02:43 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -2,13 +2,20 @@
 
 ~/stream/iss.py > /tmp/iss.log && vischeck=$(</tmp/iss.log) && visibility=$(</tmp/iss.log)
 
+case "$1" in
+ch0)
+while sleep 60; do
+~/stream/iss.py > /tmp/iss.log && visibility=$(</tmp/iss.log)
+if [ "$visibility" != "$vischeck" ]; then pkill -f livestreamer && echo -e "\e[31mstream killed due to change from $vischeck to $visibility\e[0m"; else echo -e "\e[31meverything is fine, don't panic!\e[0m"; fi
+vischeck=$visibility
+done &
+;;
+esac
+
 # keeps the livestream up and running
 while true; do
 case "$1" in
 ch0)
-~/stream/iss.py > /tmp/iss.log && visibility=$(</tmp/iss.log)
-if [ "$visibility" != "$vischeck" ]; then pkill -f livestreamer && echo -e "\e[31mstream killed due to change from $vischeck to $visibility\e[0m"; else echo -e "\e[31meverything is fine, don't panic!\e[0m"; fi
-vischeck=$visibility
 if [ $visibility != eclipsed ]; then
 /usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
 else
