75ba27f6b4292b23d465a3e5edbc9804ed2e584d
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-09-01 21:03:04 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -30,7 +30,7 @@ ch2)
 /usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
 ;;
 ch3)
-/usr/local/bin/livestreamer http://http://www.ustream.tv/channel/nasa-hd-tv mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv mobile_720p --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live {filename}"
 ;;
 esac
 done &
