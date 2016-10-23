c1d4f5077a417f859ceaca8c567333610889595f
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-10-24 00:07:41 +0200

    Update iss-stream-s.sh

--- iss-stream-s.sh
+++ iss-stream-s.sh
@@ -7,10 +7,10 @@ ch1)
 /usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_720p --player omxplayer --fifo --player-args "--win '400 0 800 240' --live {filename}"
 ;;
 ch2)
-/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo --player-args "--win '0 240 400 480' --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream mobile_480p --player omxplayer --fifo --player-args "--win '400 240 800 480' --live {filename}"
 ;;
 ch3)
-/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv mobile_720p --player omxplayer --fifo --player-args "--win '400 240 800 480' --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv mobile_720p --player omxplayer --fifo --player-args "--win '0 240 400 480' --live {filename}"
 ;;
 esac
 done
