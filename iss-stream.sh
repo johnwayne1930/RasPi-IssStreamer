2f3083d888d001f16c9b6cec09c14ad82bacf3e2
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-10-23 14:23:38 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -38,7 +38,7 @@ done &
 # orbit picture for the background
 wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"
 DISPLAY=:0.0 XAUTHORITY=~/.Xauthority /usr/bin/feh -F -Z -R 30 /tmp/issorbit.png &
-while sleep 60; do wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"; done &
+while sleep 120; do wget -O /tmp/issorbit.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544"; done &
 
 # orbit overlay in top left corner; have to simplify this..
 wget -O /tmp/ISS-Display-image1.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=150&height=150&satid=25544"
