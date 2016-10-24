2b87c79001bbbeea070f042bc148165f17f6eb96
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-10-24 19:31:21 +0200

    Update iss-stream-s.sh

--- iss-stream-s.sh
+++ iss-stream-s.sh
@@ -5,7 +5,7 @@ while true; do
 case "$1" in
 ch1)
 # mobile_240p / mobile_360p / mobile_480p / mobile_720p
-/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_240p --player omxplayer --fifo --player-args "--win '400 0 800 240' --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_240p --player omxplayer --fifo --player-args "--win '0 240 400 480' --live {filename}"
 ;;
 ch2)
 # mobile_284p / mobile_426p / mobile_480p
@@ -13,7 +13,7 @@ ch2)
 ;;
 ch3)
 # mobile_240p / mobile_360p / mobile_480p / mobile_720p
-/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv mobile_240p --player omxplayer --fifo --player-args "--win '0 240 400 480' --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv mobile_240p --player omxplayer --fifo --player-args "--win '400 0 80 240' --live {filename}"
 ;;
 esac
 done
