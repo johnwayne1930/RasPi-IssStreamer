f31b474048df220d6895bb24419aeb72e864c6e5
johnwayne1930 <doppelhaushelfte@yahoo.de>
2016-10-25 19:41:47 +0200

    Update iss.sh

--- iss.sh
+++ iss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killstream () {
-for session in $(screen -ls | grep -o '[0-9]\{5\}') # or maybe?: for session in $(screen -ls | grep -o '[0-9]\+')
+for session in $(screen -ls | grep -o '[0-9]\+') # or maybe?: for session in $(screen -ls | grep -o '[0-9]\{5\}') 
 do
 screen -S "${session}" -X quit;
 done
