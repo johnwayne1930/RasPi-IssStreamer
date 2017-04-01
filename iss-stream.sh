c5cd361d617f9de78e63169f5e2ab8b69fbceaf8
johnwayne1930 <doppelhaushelfte@yahoo.de>
2017-04-01 20:42:09 +0200

    Update iss-stream.sh

--- iss-stream.sh
+++ iss-stream.sh
@@ -18,19 +18,19 @@ case "$1" in
 ch0)
 ~/stream/iss.py > /tmp/iss.log && visibility=$(</tmp/iss.log)
 if [ $visibility != eclipsed ]; then
-/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --timeout 25 --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live --timeout 25"
 else
-/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream best --player omxplayer --fifo  --player-args "--layer 1000 --win '0 0 800 480' --timeout 25 --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream best --player omxplayer --fifo  --player-args "--layer 1000 --win '0 0 800 480' --live --timeout 25"
 fi
 ;;
 ch1)
-/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --timeout 25 --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/iss-hdev-payload best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live --timeout 25"
 ;;
 ch2)
-/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --timeout 25 --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/live-iss-stream best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live --timeout 25"
 ;;
 ch3)
-/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --timeout 25 --live {filename}"
+/usr/local/bin/livestreamer http://ustream.tv/channel/nasa-hd-tv best --player omxplayer --fifo --player-args "--layer 1000 --win '0 0 800 480' --live --timeout 25"
 ;;
 esac
 done
