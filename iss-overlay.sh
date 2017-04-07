4cb550373115e929511a1ff5d612994c0972c5cf
johnwayne1930 <doppelhaushelfte@yahoo.de>
2017-04-07 16:35:11 +0200

    Update iss-overlay.sh

--- iss-overlay.sh
+++ iss-overlay.sh
@@ -10,39 +10,39 @@ avconv -loop 1 -i /tmp/ISS-Display-image1.png -tune stillimage -t 10 -y -r 5 /tm
 while true; do
 (wget -O /tmp/ISS-Display-image2.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=150&height=150&satid=25544";
 avconv -loop 1 -i /tmp/ISS-Display-image2.png -tune stillimage -t 10 -y -r 5 /tmp/ISS-Display-video2.mp4) &
-(omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+(omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 1100 --win "0 0 80 80" ) &
+omxplayer -o hdmi /tmp/ISS-Display-video1.mp4 --layer 2 --win "0 0 80 80" ) &
 sleep 60
  
 (wget -O /tmp/ISS-Display-image1.png "http://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=150&height=150&satid=25544";
 avconv -loop 1 -i /tmp/ISS-Display-image1.png -tune stillimage -t 10 -y -r 5 /tmp/ISS-Display-video1.mp4) &
-(omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+(omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80" &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
-omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 1200 --win "0 0 80 80") &
+omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80") &
 sleep 60
 done
