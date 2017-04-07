1b6ca85f2a9f8151508d14aa6751facce0f6ee76
johnwayne1930 <doppelhaushelfte@yahoo.de>
2017-04-07 17:13:57 +0200

    Update sun-overlay.sh

--- sun-overlay.sh
+++ sun-overlay.sh
@@ -1,4 +1,4 @@
-# orbit overlay in top left corner; have to simplify this..
+# sun overlay in lower left corner; have to simplify this..
 wget -O /tmp/SUN-Display-image1.jpg "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg"
 avconv -loop 1 -i /tmp/SUN-Display-image1.jpg -tune stillimage -t 10 -y -r 5 /tmp/SUN-Display-video1.mp4
  
@@ -23,7 +23,7 @@ omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 2 --win "0 0 80 80" ) &
 sleep 60
  
 (wget -O /tmp/ISS-Display-image1.jpg "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg";
-avconv -loop 1 -i /tmp/SUN-Display-image1.jpg -tune stillimage -t 10 -y -r 5 /tmp/ISS-Display-video1.mp4) &
+avconv -loop 1 -i /tmp/SUN-Display-image1.jpg -tune stillimage -t 10 -y -r 5 /tmp/SUN-Display-video1.mp4) &
 (omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
 omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
