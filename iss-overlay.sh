b515307fc11afaecbce1952db8f826fce752eff5
johnwayne1930 <doppelhaushelfte@yahoo.de>
2017-04-07 16:48:33 +0200

    Update iss-overlay.sh

--- iss-overlay.sh
+++ iss-overlay.sh
@@ -45,48 +45,4 @@ omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80" &
 sleep 9;
 omxplayer -o hdmi /tmp/ISS-Display-video2.mp4 --layer 3 --win "0 0 80 80") &
 sleep 60
-done &
-
-# orbit overlay in top left corner; have to simplify this..
-wget -O /tmp/SUN-Display-image1.png "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg"
-avconv -loop 1 -i /tmp/SUN-Display-image1.png -tune stillimage -t 10 -y -r 5 /tmp/SUN-Display-video1.mp4
- 
-while true; do
-(wget -O /tmp/SUN-Display-image2.png "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg";
-avconv -loop 1 -i /tmp/SUN-Display-image2.png -tune stillimage -t 10 -y -r 5 /tmp/SUN-Display-video2.mp4) &
-(omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video1.mp4 --layer 4 --win "0 0 80 80" ) &
-sleep 60
- 
-(wget -O /tmp/SUN-Display-image1.png "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg";
-avconv -loop 1 -i /tmp/SUN-Display-image1.png -tune stillimage -t 10 -y -r 5 /tmp/SUN-Display-video1.mp4) &
-(omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480" &
-sleep 9;
-omxplayer -o hdmi /tmp/SUN-Display-video2.mp4 --layer 3 --win "0 400 80 480") &
-sleep 60
 done
